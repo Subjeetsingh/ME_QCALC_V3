7d27712df99bb41eb0fafa59d21e7ef6747d120c
CrioUser <crio-user@criodo.com>
2023-06-21 13:04:02 +0530

    ME_QCALC_V3_MODULE_IMPLEMENTATION

--- app/src/main/java/com/crio/qcalc/App.java
+++ app/src/main/java/com/crio/qcalc/App.java
@@ -9,8 +9,22 @@ public class App {
         return "Hello World!";
     }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-    }
+    // public static void main(String[] args) {
+    //     System.out.println(new App().getGreeting());
+    
+   public static void main(String[] args) {
+
+    System.out.println("Starting QCalc..");
+
+    StandardCalculator calc = new StandardCalculator();
+
+    calc.add(1, 2);
+
+    System.out.println(calc.getResult());
+
 }
+
+
+}
+
 
--- app/src/main/java/com/crio/qcalc/StandardCalculator.java
+++ app/src/main/java/com/crio/qcalc/StandardCalculator.java
@@ -1,12 +1,61 @@
 package com.crio.qcalc;
 
-public class StandardCalculator {
 
-    public static void getVersion(){
-        System.out.println("Standard Calculator 1.0");
-    }
+public class StandardCalculator
+{
+   public static void getVersion(){
+       System.out.println("Standard Calculator 1.0");
+   }
+
+
+   private int result;
+
+
+   public int getResult() {
+       return result;
+   }
+      
+   public void clearResult() {
+       result = 0;
+   }
+  
+//    public void printResult(int result){
+//       // int res = getResult(result);
+//        System.out.println("Standard Calculator Result:"+ res);
+//    }
 
 
 
 
+   private void setResult(int value) {
+      this.result = value;
+   }
+
+
+   public void add(int num1, int num2){
+       result =(num1+num2);
+   }
+
+
+
+
+   public void subtract(int num1, int num2){
+       setResult(num1-num2);
+   }
+  
+  
+   public void multiply(int num1, int num2){
+       setResult(num1*num2);
+   }
+  
+  
+   public void divide(int num1, int num2){
+       if(num2!=0){
+           setResult(num1/num2);
+       }
+       else{
+           setResult(0);
+       }
+   }
 }
+
